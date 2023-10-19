b284fac3b64bbb87c6427ba24f24d715a5206e1b
dedmc95@gmail.com <dedmc95@gmail.com>
2023-10-18 22:22:56 -0700

    edit

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,19 @@
+/* DOM to retrieve temperature and wind speed from HTML */
+document.querySelector('#temperature').value = Temperature;
+document.querySelector('#windSpeed').value = windSpeed;
+
+/* Windchill formula */
+let windChill = 35.74 + (0.62150 * Temperature) - ((35.75 * windSpeed) ** 0.16) + ((0.4275 * Temperature * windSpeed) ** 0.16);
+
+/* event handler */
+addEventListener("keypress", )
+
+/* DOM to insert Windchill formula result*/
+document.querySelector('#windChill').innerHTML = windChill;
+/* if statement for temperature */
+if (Temperature <= 50 && windSpeed > 3.0) {
+    console.log(windChill);
+}
+else {
+    console.log("N/A");
+}
