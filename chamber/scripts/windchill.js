1db45525ab857debc9f0b4c0679b50cddf3a5d42
dedmc95@gmail.com <dedmc95@gmail.com>
2023-10-19 18:05:02 -0700

    chamber assignment windchill

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -8,14 +8,16 @@ let windChill = 35.74 + (0.62150 * Temperature) - ((35.75 * windSpeed) ** 0.16) 
 /* DOM function to insert Windchill formula result*/
 function windChillCal() {
     document.querySelector('#windChill').innerHTML = windChill;
+    
+    /* if statement for temperature */
+    if (Temperature <= 50 && windSpeed > 3.0) {
+    console.log(windChill);
+    }
+    else {
+        console.log("N/A");
+    }
 }
 /*event listener for when screen is clicked.*/
 document.addEventListener('click', windChillCal);
 
-/* if statement for temperature */
-if (Temperature <= 50 && windSpeed > 3.0) {
-    console.log(windChill);
-}
-else {
-    console.log("N/A");
-}
+
